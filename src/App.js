2c204f3d1945d0a8fa75e7ef45c0424c08abe711
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-04-17 14:42:46 +0530

    Expense component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItems from "./Components/ExpenseItems";
+
+
+function App() {
+  return (
+    <div>
+      <ExpenseItems/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems(){
+    return(<>
+        <h1>Expense Items</h1>
+        <ul>
+            <li>Food Rs 10</li>
+            <li>Petrol Rs 100</li>
+            <li>Movies Rs 200</li>
+        </ul>
+        </>
+    )
+}
+
+export default ExpenseItems
