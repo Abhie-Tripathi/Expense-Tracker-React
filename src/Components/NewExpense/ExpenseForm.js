1f0108a70fcaf5999e3713f3b16957f352b22b74
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-04-19 16:22:52 +0530

    Introducing

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,29 +2,40 @@ import React,{useState} from "react";
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-    const [text,settext] = useState("")
-    const changehandler = (e) =>{
-        settext(e.target.value)
-        console.log(text)
+    const [enteredtitle,setenteredtitle] = useState("")
+    const titlechangehandler = (e) =>{
+        setenteredtitle(e.target.value)
+    }
+    const [enteredamount,setenteredamount] = useState("")
+    const amountchangehandler = (e) =>{
+        setenteredamount(e.target.value)
+    }
+    const [enteredlocation,setenteredlocation] = useState("")
+    const locationchangehandler = (e) =>{
+        setenteredlocation(e.target.value)
+    }
+    const [entereddate,setentereddate] = useState("")
+    const datechangehandler = (e) =>{
+        setentereddate(e.target.value)
     }
 
     return(<form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={changehandler} value={text}/>
+                <input type="text" onChange={titlechangehandler}/>
             </div>
             <div className="new-expense__control">
                 <label>location</label>
-                <input type="text"/>
+                <input type="text" onChange={locationchangehandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={datechangehandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" onChange={amountchangehandler}/>
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
