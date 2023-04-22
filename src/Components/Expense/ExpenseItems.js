eaf909f7c1f3966119be01a62ded2ca05e42794c
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-04-22 11:02:20 +0530

    Conditional Rendering

--- src/Components/Expense/ExpenseItems.js
+++ src/Components/Expense/ExpenseItems.js
@@ -16,17 +16,23 @@ const ExpenseItems = (props) => {
     const filtered = props.object.filter((obj)=>{
         return(obj.date.getFullYear().toString()===filteredYear)
     })
-        
+
+    let anyfiltered = <p id="anyfiltered">No Item Found</p>
+    if(filtered.length>0 ){
+        anyfiltered = filtered.map(function(obj){
+            return( <><div className="expense-item" key={obj.id}>
+            <ExpenseDate date ={obj.date}/>
+            <ExpenseDetails type = {obj.type} amount = {obj.amount} location = {obj.location}/>
+            </div> 
+            {filtered.length===1 ? (<p id="anyfiltered">Add More Expenses</p>):(<></>)}
+            </>
+            )})
+    }
     
     return (<>
         <div className="expenses">
         <ExpensesFilter filter={getfilter} selectedYear={filteredYear}/>
-        
-        {filtered.map(function(obj){
-        return( <div className="expense-item" key={obj.id}>
-        <ExpenseDate date ={obj.date}/>
-        <ExpenseDetails type = {obj.type} amount = {obj.amount} location = {obj.location}/>
-        </div> )})}
+        {anyfiltered}
         </div>
         </>)}
             
